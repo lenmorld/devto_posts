ede7cba511b4390ec24522be096939df414684e8
lenmorld <lenmorld@gmail.com>
2019-05-07 22:02:06 -0400

    90% done

--- quick_node_express_diskdb/server.js
+++ quick_node_express_diskdb/server.js
@@ -1,29 +1,20 @@
 const express = require('express');
 const server = express();
-let data = require('./data');
 
 const body_parser = require('body-parser');
 
 const port = 4000;
 
-const mongodb = require('mongodb');
+// diskdb connection
+const db = require('diskdb');
+db.connect('./data', ['movies']);
 
-const MongoClient = mongodb.MongoClient;
-const uri = "mongodb+srv://user1:abcd1234@cluster0-r1jy0.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("app_db").collection("movies_collection");
-
-  collection.find().toArray((err, result) => {
-      if (err) throw err;
-      console.log(result);
-      // res.json(result);
-   });
-
-  // perform actions on the collection object
-  client.close();
-});
-
+// add first movie
+if (!db.movies.find().length) {
+   const movie = { id: "tt0110357", name: "The Lion King", genre: "animation" };
+   db.movies.save(movie);
+}
+console.log(db.movies.find());
 
 // parse JSON (application/json content-type)
 server.use(body_parser.json());
@@ -33,17 +24,16 @@ server.get("/", (req, res) => {
 });
 
 server.get("/items", (req, res) => {
-   res.json(data);
+   res.json(db.movies.find());
 });
 
 server.get("/items/:id", (req, res) => {
    const itemId = req.params.id;
-   const item = data.find(_item => _item.id === itemId);
-
-   if (item) {
-      res.json(item);
+   const items = db.movies.find({ id: itemId });
+   if (items.length) {
+      res.json(items);
    } else {
-      res.json({ message: `item ${itemId} doesn't exist`})
+      res.json({ message: `item ${itemId} doesn't exist` })
    }
 });
 
@@ -51,11 +41,11 @@ server.post("/items", (req, res) => {
    const item = req.body;
    console.log('Adding new item: ', item);
 
-   // add new item to array
-   data.push(item)
-  
+   // add new item to db
+   db.movies.save(item);
+
    // return updated list
-   res.json(data);
+   res.json(db.movies.find());
 });
 
 // update an item
@@ -64,39 +54,22 @@ server.put("/items/:id", (req, res) => {
    const item = req.body;
    console.log("Editing item: ", itemId, " to be ", item);
 
-   const updatedListItems = [];
-   // loop through list to find and replace one item
-   data.forEach(oldItem => {
-      if (oldItem.id === itemId) {
-         updatedListItems.push(item);
-      } else {
-         updatedListItems.push(oldItem);
-      }
-   });
+   db.movies.update({ id: itemId }, item);
 
-   // replace old list with new one
-   data = updatedListItems;
-
-   res.json(data);
+   res.json(db.movies.find());
 });
 
 // delete item from list
-// delete item from list
 server.delete("/items/:id", (req, res) => {
    const itemId = req.params.id;
-
    console.log("Delete item with id: ", itemId);
 
-   // filter list copy, by excluding item to delete
-   const filtered_list = data.filter(item => item.id !== itemId);
+   db.movies.remove({ id: itemId });
 
-   // replace old list with new one
-   data = filtered_list;
-
-   res.json(data);
+   res.json(db.movies.find());
 });
 
 
 server.listen(port, () => {
-    console.log(`Server listening at ${port}`);
+   console.log(`Server listening at ${port}`);
 });
